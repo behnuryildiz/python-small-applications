7f953cf47927d0296cd990c38fb92d2151d8257c
Behnur Yildiz <behnuryildiz@hotmail.com>
2023-05-29 15:09:42 +0200

    loan calculator created.

---
+++ loanCalculator.py
@@ -0,0 +1,22 @@
+# Loan Calculator
+
+loanedMoney = float(input("How many do you need to load? "))
+howLong = int(input("How long do you need to pay it back ( in years ) ? "))
+interest = 5
+print(f"the interest is for 10 years {interest}%")
+
+calculationResultList = []
+for year in range(howLong):
+    calculationOfResultInterest = (loanedMoney * interest * (year + 1) / 100)
+    calculationResultList.append(calculationOfResultInterest)
+
+    print(f"Year {year+1} is ", (loanedMoney + calculationOfResultInterest), "$")
+print(
+    f"You paid for interest for {howLong} years totally : {sum(calculationResultList)}$"
+)
+"""A main money(capital)
+t interest for year,
+n time in year,
+F interest; F=A.n.t/100
+If we want to calculate month based; F=A.n.t1200
+"""
